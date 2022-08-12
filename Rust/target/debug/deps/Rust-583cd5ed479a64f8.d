e9353070d1a6d4a385b4a04e6a41b98fe5e74f90
Nour Nagm <nournagm97@gmail.com>
2022-08-12 20:00:11 +0200

    deleted incomplete startup folders , add when needed

--- Rust/target/debug/deps/Rust-583cd5ed479a64f8.d
+++
@@ -1,5 +0,0 @@
-e:\code\Katas\Parrot-Refactoring-Kata\Rust\target\debug\deps\Rust-583cd5ed479a64f8.rmeta: src\lib.rs
-
-e:\code\Katas\Parrot-Refactoring-Kata\Rust\target\debug\deps\Rust-583cd5ed479a64f8.d: src\lib.rs
-
-src\lib.rs:
--- Rust/target/debug/deps/Rust-65a99aaee5da63ac.d
+++
@@ -1,5 +0,0 @@
-e:\code\Katas\Parrot-Refactoring-Kata\Rust\target\debug\deps\Rust-65a99aaee5da63ac.rmeta: src\lib.rs
-
-e:\code\Katas\Parrot-Refactoring-Kata\Rust\target\debug\deps\Rust-65a99aaee5da63ac.d: src\lib.rs
-
-src\lib.rs:
